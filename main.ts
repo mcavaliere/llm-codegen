0bf34775744228c9b0b6931fbfd0a9315dcd9d28
Mike Cavaliere <mcavaliere@users.noreply.github.com>
2024-05-21 17:54:26 -0400

    feat: basic CLI args, md docs for shadcn

--- main.ts
+++ main.ts
@@ -1,9 +1,24 @@
 import OpenAI from "openai";
+import { Command } from 'commander'
 import 'dotenv/config'
 
 if (!process.env.OPENAI_API_KEY) {
   throw new Error("OPENAI_API_KEY not found");
 }
+
+const program = new Command();
+
+program
+  .name('shadgen')
+  .description('LLM component generation for Shadcn-UI')
+  .version('0.0.1')
+
+program
+  .command('gen')
+  .description('Generate the code for a component')
+  .argument('<component>', 'The name of the component')
+
+  program.parse(process.argv);
 
 const openai = new OpenAI();
 
@@ -13,7 +28,7 @@ async function main() {
     model: 'gpt-3.5-turbo',
   });
 
-  console.log(`---------------- chatCompletion: `, chatCompletion);
+  console.log(`---------------- chatCompletion: `, chatCompletion.choices[0].message.content);
 }
 
 main();
