844a90c54c9adc3b8a6747c16717c4fd0f310984
Mike Cavaliere <mcavaliere@users.noreply.github.com>
2024-06-11 13:51:45 -0400

    chore: update prompt

--- commands.ts
+++ commands.ts
@@ -1,10 +1,10 @@
 import fs from "fs";
 import OpenAI from "openai";
-import { inspect } from "util";
 
 export const systemPrompt = `You're an expert full-stack software engineer with vast knowledge of TypeScript, JavaScript and React. You're an expert with the Shadcn-ui library. I'm going to provide the documentation for a single component in the shadcn-ui library and then ask you to do something with it. Don't do anything with it and don't respond to this prompt.`;
 export function docPrompt(componentName: string, fileContent: string) {
-  return `This is the documentation for the ${componentName} component in the shadcn-ui library:
+  return `This is the documentation for the ${componentName} component in the shadcn-ui library. It contains instructions for installation and code examples. Return the relevant portions if I ask you to generate a ${componentName}.
+
   <DOC>
     ${fileContent}
   </DOC>
